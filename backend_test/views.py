fbbfeb90de641e7a83aef7c82b3ff275e69c6016
danny7128 <danny7128@gmail.com>
2022-11-29 14:27:45 +0900

    TEST : 카카오map 연습 [이성훈]

--- backend_test/views.py
+++ backend_test/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "backend_test/index.html")
+
+def map(request):
+    return render(request, "backend_test/map.html")
+
+def map2(request):
+    return render(request, "backend_test/map2.html")
