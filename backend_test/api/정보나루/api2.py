3f53923ead5a393d6e4d083d62b8e5ec12fd2aed
danny7128 <danny7128@gmail.com>
2022-11-29 10:29:36 +0900

    TEST : 알라딘,정보나루 api사용 예제 [이성훈]

---
+++ backend_test/api/정보나루/api2.py
@@ -0,0 +1,93 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+api_key = "c30a173104dbac8b7924d537a3ec76270dda1187394b8cd8a8f2ae3b1bdd509a"
+isbn = "&isbn13=" + "9788983921987"
+format = "&format=" + "xml"
+
+
+# ===================
+libCode = "&libCode=" + "141321"
+type = "&type=" + "ALL"
+pageNo = "&pageNo=" + "1"
+pageSize = "&pageSize=" + "100"
+
+def library():
+    URL = (
+    "http://data4library.kr/api/itemSrch?authKey=" 
+    + api_key 
+    + libCode 
+    + type
+    + pageNo
+    + pageSize
+    + format
+    )
+    
+    # print(URL)
+    response = requests.get(URL)
+    
+    soup = BeautifulSoup(response.text, "xml")
+    bookIds = soup.find_all("isbn13")
+
+    bookIds_list = []
+    for id in bookIds:
+        bookIds_list.append(id.get_text())
+    
+    return bookIds_list
+
+library()
+
+
+def book_info():
+    for bookId in library():
+    
+        URL = (
+        "http://data4library.kr/api/srchDtlList?authKey=" 
+        + api_key 
+        + "&isbn13=" + bookId
+        + format
+        )
+        print(URL)
+
+        response = requests.get(URL)
+
+        soup = BeautifulSoup(response.text, "xml")
+        _no = soup.find("no").get_text()
+        _bookname = soup.find("bookname").get_text()
+        _authors = soup.find("authors").get_text()
+        _publisher = soup.find("publisher").get_text()
+        _class_no = soup.find("class_no").get_text()
+        _class_nm = soup.find("class_nm").get_text()
+        _publication_year = soup.find("publication_year").get_text()
+        _publication_date = soup.find("publication_date").get_text()
+        _bookImageURL = soup.find("bookImageURL").get_text()
+        _description = soup.find("description").get_text()
+
+        print(_no)
+        print(_bookname)
+        print(_authors)
+        print(_publisher)
+        print(_class_no)
+        print(_class_nm)
+        print(_publication_year)
+        print(_publication_date)
+        print(_bookImageURL)
+        print(_description)
+
+        # import model
+        # book = model(
+        #     no = _no,
+        #     bookname = _bookname,
+        #     authors = _authors,
+        #     publisher = _publisher,
+        #     class_no = _class_no,
+        #     class_nm = _class_nm,
+        #     publication_year = _publication_year,
+        #     publication_date = _publication_date,
+        #     bookImage = _bookImageURL,
+        #     description = _description,
+        # )
+        # book.save()
+
+book_info()
