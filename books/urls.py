1e017d6dd3e04ac229d9f3eb95d1196bf47a55e9
ericaforcoding <ericaforcoding@gmail.com>
2022-11-30 16:35:56 +0900

    DESIGN Onboarding main image [진수]

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("onboarding/", views.onboarding, name="onboarding"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book
 
+def onboarding(request):
+    return render(request, "books/onboarding.html")
 
 def index(request):
     books = Book.objects.all()
