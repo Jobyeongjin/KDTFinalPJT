8b711053d9e6fe473665cef6f074087bacb8418a
danny7128 <danny7128@gmail.com>
2022-12-02 11:43:48 +0900

    FEAT Groups index 조회,detail 추가 [성훈]

--- groups/urls.py
+++ groups/urls.py
@@ -6,5 +6,6 @@ app_name = "groups"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("<int:pk>/detail/", views.detail, name="detail"),
 
 ]
--- groups/views.py
+++ groups/views.py
@@ -9,7 +9,11 @@ from django.contrib.auth.decorators import login_required
 
 # 모임 페이지
 def index(request):
-    return render(request, "groups/index.html")
+    groups = Group.objects.all()
+    context = {
+        "groups":groups,
+    }
+    return render(request, "groups/index.html",context)
 
 
 # 모임 생성
@@ -27,3 +31,11 @@ def create(request):
             group_form = GroupForm()
         context = {"group_form": group_form}
         return render(request, "groups/create.html", context)
+
+# 책 리뷰 디테일(댓글 추가 전)
+def detail(request, pk):
+    group = Group.objects.get(pk=pk)
+    context = {
+        "group":group,
+    }
+    return render(request, "groups/detail.html", context)
