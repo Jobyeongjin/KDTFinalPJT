6fd619d762dca04e8ba400ded7399150c6790f8f
byeongjin <qudwls7468@gmail.com>
2022-12-02 18:03:43 +0900

    REFACTOR URL 주소 변경 [병진]

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("books.urls")),
     path("accounts/", include("accounts.urls")),
-    path("books/", include("books.urls")),
     path("reviews/", include("reviews.urls")),
     path("groups/", include("groups.urls")),
     path("backend_test/", include("backend_test.urls")),
--- reviews/urls.py
+++ reviews/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("<int:pk>/detail/", views.detail, name="detail"),
-    path("<int:pk>/review_update", views.update, name="update"),
-    path("<int:pk>/review_delete", views.delete, name="delete"),
+    path("<int:pk>/update", views.update, name="update"),
+    path("<int:pk>/delete", views.delete, name="delete"),
     path("<int:pk>/comment/create/", views.comment_create, name="comment_create"),
-    path("<int:review_pk>/<int:comment_pk>/comment/delete/", views.comment_delete, name="comment_delete"),
+    path(
+        "<int:review_pk>/comment/<int:comment_pk>/delete/",
+        views.comment_delete,
+        name="comment_delete",
+    ),
 ]
