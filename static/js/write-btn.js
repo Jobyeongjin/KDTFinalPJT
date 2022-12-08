0070919e46081e557c36ed2ec4c0fe67bd39097b
byeongjin <qudwls7468@gmail.com>
2022-12-08 21:03:59 +0900

    UPDATE Books main 글쓰기 버튼 스크롤시 나타내기 추가 [병진]

---
+++ static/js/write-btn.js
@@ -0,0 +1,22 @@
+// top-tutton fade-in
+$(document).ready(function(){
+  let navHeight = $(".nav").height(); 
+  //navHeight 의 높이를 구하기
+  $(".write-gb").hide();
+  //스크롤시 나타날 객체 미리 숨기기
+  $(window).scroll(function(){  // 윈도우 스크롤 기능 작동
+      let rollIt = $(this).scrollTop() >= navHeight + 500; 
+// 스크롤의 정도가 navHeight 의 값을 넘었을 때 == 윈도우 스크롤의 값이 navHeight 의 높이와 같거나 크다
+/*
+scrollTop 은 윈도우에서 스크롤의 위치가 가장 상위에 있다는 의미로 해석
+스크롤의 위치가 화면 아래일수록 == scrollTop 의 값이 커짐
+*/
+  if(rollIt){ 
+  //윈도우 스크롤 기능의 값이 navHeight 의 높이와 같거나 크면
+          $(".write-gb").show().css({"position":"fixed"});
+      }
+      else{
+          $(".write-gb").hide();
+      }
+  });
+});
