5dfebfe5a20c0e437e0d02448e83cd1535f20a28
byeongjin <qudwls7468@gmail.com>
2022-12-08 22:31:13 +0900

    DESIGN Books search 검색창 포커스시 색상 변경 [병진]

--- static/js/main.js
+++ static/js/main.js
@@ -7,12 +7,10 @@ searchEl.addEventListener('click', function() {
 });
 
 searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
 searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
 
