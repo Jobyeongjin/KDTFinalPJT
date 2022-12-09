3144fa237dc365ce766fed45f17db28322b1f4dc
byeongjin <qudwls7468@gmail.com>
2022-12-09 15:03:16 +0900

    FEAT Reviews index 리뷰 태그 및 생성시간 포맷팅 [병진]

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from books.models import Book
 from taggit.managers import TaggableManager
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 
 class Book_Review(models.Model):
@@ -14,6 +16,22 @@ class Book_Review(models.Model):
     like_user = models.ManyToManyField(get_user_model(), related_name="like_review")
     bookId = models.ForeignKey(Book, on_delete=models.CASCADE)
     tags = TaggableManager(blank=True)
+
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return "방금 전"
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + "분 전"
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + "시간 전"
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + "일 전"
+        else:
+            return False
 
 
 class Book_Review_Comment(models.Model):
