33955fd530b8dac989a9b07f31a79c348a65c918
minw0095 <kmu6898@naver.com>
2022-12-09 18:27:38 +0900

    UPDATE 각 페이지 별 리뷰 작성 이동 수정[민우]

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,8 +5,8 @@ app_name = "reviews"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create_txt/", views.create_txt, name="create_txt"),
-    path("create_img/", views.create_img, name="create_img"),
+    path("<int:book_pk>/create_txt/", views.create_txt, name="create_txt"),
+    path("<int:book_pk>/create_img/", views.create_img, name="create_img"),
     path("<int:pk>/detail/", views.detail, name="detail"),
     path("<int:pk>/like/", views.like, name="like"),
     path("<int:pk>/update", views.update, name="update"),
@@ -19,5 +19,6 @@ urlpatterns = [
         views.comment_delete,
         name="comment_delete",
     ),
-    path("create/", views.create, name="create")
+    path("pk_create_txt/", views.pk_create_txt, name="pk_create_txt"),
+    path("pk_create_img/", views.pk_create_img, name="pk_create_img"),
 ]
