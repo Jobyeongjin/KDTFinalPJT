df00bfec880e438dda23babc35543fc47ee982e5
ericaforcoding <ericaforcoding@gmail.com>
2022-12-11 21:51:10 +0900

    DEPLOY test 4 [진수]

--- books/models.py
+++ books/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 
 class Book(models.Model):
-    bookname = models.CharField(max_length=200)
-    authors = models.CharField(max_length=200)
-    publisher = models.CharField(max_length=200)
-    publication_year = models.CharField(max_length=100)
-    class_nm = models.CharField(max_length=100)
-    bookcover = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    bookname = models.CharField(max_length=1000)
+    authors = models.CharField(max_length=1000)
+    publisher = models.CharField(max_length=1000)
+    publication_year = models.CharField(max_length=1000)
+    class_nm = models.CharField(max_length=2000)
+    bookcover = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
     like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="books")
