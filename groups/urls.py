c2224740c7be97b0a72fd397be26dab216919811
byeongjin <qudwls7468@gmail.com>
2022-12-11 17:11:08 +0900

    STYLE URL 코드 순서 변경 [병진]

--- groups/urls.py
+++ groups/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("<int:pk>/detail/", views.detail, name="detail"),
+    path("<int:pk>/update/", views.update, name="update"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
     path("<int:pk>/like/", views.like, name="like"),
-    path("<int:pk>/review_update", views.update, name="update"),
-    path("<int:pk>/review_delete", views.delete, name="delete"),
     path("<int:pk>/group_closed/", views.group_closed, name="group_closed"),
 ]
